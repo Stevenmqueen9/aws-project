2af915389d5f6b75ebda55ff744b69a2150ee118
Stevenmqueen9 <118812416+Stevenmqueen9@users.noreply.github.com>
2024-05-24 01:53:56 +0200

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,7 +1,7 @@
 <?php
-$hostname="localhost";
+$hostname="database:3306";
 $username="root";
-$password="";
+$password="root";
 $database="aws";
 
 $conn=new mysqli($hostname,$username,$password,$database);
